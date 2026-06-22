2e6a3b781137ecd6f6f0b8914296d1e4418301f7
Jean Leroy <jean.leroy@inrae.fr>
2026-06-22 21:58:44 +0200

    Add files via upload

--- highlight_nav.js
+++ highlight_nav.js
@@ -1,13 +1,23 @@
-$(document).ready(function() {
-    setNavigation();
+function myFun () {
+    document.querySelectorAll(".nav-link").forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+    }
 });
-
-function setNavigation() {
-    var path = $(location).attr('pathname');
-    $("nav#main-nav a").each(function() {
-        var href = $(this).attr('href');
-        if (path == href) {
-            $(this).addClass('active');
-        }
-    });
 }
+
+
+// $(document).ready(function() {
+//     setNavigation();
+// });
+
+// function setNavigation() {
+//     var path = $(location).attr('pathname');
+//     $("nav#main-nav a").each(function() {
+//         var href = $(this).attr('href');
+//         if (path == href) {
+//             $(this).addClass('active');
+//         }
+//     });
+// }
